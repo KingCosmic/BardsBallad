bb63b7ed5c6edcc67b70df7d29142569e65345e7
KingCosmic <KingCosmicDe@gmail.com>
2019-09-13 04:18:03 -0500

    fixed issues

---
+++ build/precache-manifest.09d7dc7c36a8d935da76f3fa6481386b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "fcdec307dd2978334ec6",
+    "url": "/static/css/main.4b9b1700.chunk.css"
+  },
+  {
+    "revision": "0b7f74978ad6f62496db",
+    "url": "/static/js/1.0b7f7497.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "889c5cf0c4b919bfb66d09df46d314dd",
+    "url": "/static/media/Nunito-Light.889c5cf0.ttf"
+  },
+  {
+    "revision": "11781884e3a96b39d8c6cd0e2bdaa40a",
+    "url": "/static/media/Nunito-Regular.11781884.ttf"
+  },
+  {
+    "revision": "507906229d794aee1c5be34e1b0fd054",
+    "url": "/static/media/Nunito-Bold.50790622.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "fcdec307dd2978334ec6",
+    "url": "/static/js/main.fcdec307.chunk.js"
+  },
+  {
+    "revision": "8bac22ed4fd7c8a30536be18e2984f84",
+    "url": "/static/media/OpenSans-ExtraBold.8bac22ed.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "3ae688ca10eb12eb5e395c1e216d1ded",
+    "url": "/static/media/patreon.3ae688ca.png"
+  },
+  {
+    "revision": "f7213e1dda669c5cda41024d9df8ea14",
+    "url": "/static/media/delete.f7213e1d.svg"
+  },
+  {
+    "revision": "c8faf3be6ff6c33c57098ff46788e5c5",
+    "url": "/static/media/edit.c8faf3be.svg"
+  },
+  {
+    "revision": "ea1ec688087230bfa7b6c9ac842d16f8",
+    "url": "/index.html"
+  }
+];
