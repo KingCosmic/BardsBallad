f2ceb5a789e0b10309a1f4d3776f557d6d0ad650
Austyn Studdard <KingCosmicDev@gmail.com>
2025-05-15 22:39:45 -0400

    added some small onboarding

---
+++ src/modals/WelcomeMessage.tsx
@@ -0,0 +1,48 @@
+import Button from '../components/inputs/Button';
+import Modal from '../components/Modal';
+import ModalBody from '../components/Modal/Body';
+import ModalFooter from '../components/Modal/Footer';
+import ModalHeader from '../components/Modal/Header';
+
+type Props = {
+  data: string | null;
+  title?: string;
+
+  isOpen: boolean;
+  requestClose(): void;
+  onSave(newData: boolean): void;
+  onDelete?(): void;
+}
+
+const WelcomeMessage: React.FC<Props> = ({ data, title = 'Edit string', isOpen, requestClose, onSave, onDelete }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={requestClose}>
+      <ModalHeader title={title}onClose={requestClose} />
+
+      <ModalBody>
+        <div className='flex flex-col gap-2'>
+          <p className='text-sm text-gray-500'>Welcome to BardsBallad we're a System Agnostic TTRPG Character Manager</p>
+          <p className='text-sm text-gray-500'>Seems you're new here. Would you like to take a tour?</p>
+        </div>
+      </ModalBody>
+
+      <ModalFooter>
+        <Button color='light'
+          onClick={() => {
+            onDelete!()
+            requestClose()
+          }}
+        >
+          No Thank You
+        </Button>
+
+        <Button color='primary' onClick={() => {
+          onSave(false)
+          requestClose()
+        }}>Yes Please!</Button>
+      </ModalFooter>
+    </Modal>
+  )
+}
+
+export default WelcomeMessage
