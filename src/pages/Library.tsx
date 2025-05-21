45afe2a0aaee1f66de516b0da8cd1fb78c1b2153
Austyn Studdard <KingCosmicDev@gmail.com>
2025-05-21 14:18:05 -0400

    added datapack section to library

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -32,6 +32,13 @@ const Library: React.FC = () => {
                   <SubscriptionCard key={sys.local_id} subscription={sys} />
                 ))}
               </div>
+
+              <h4 className='mb-2 text-xl'>Subscribed Datapacks</h4>
+              <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+                {subscriptions.filter(sub => sub.resource_type === 'datapack').map((sys) => (
+                  <SubscriptionCard key={sys.local_id} subscription={sys} />
+                ))}
+              </div>
             </div>
           </h5>
         )}
